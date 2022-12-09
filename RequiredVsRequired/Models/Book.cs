6a9e866d601e55a82c04f5e12e512754ff1c32ec
Jasper Kent <software@jasperkent.com>
2022-12-09 08:44:06 +0000

    Add project files.

---
+++ RequiredVsRequired/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RequiredVsRequired.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public required string Title { get; set; }
+        public int? Year { get; set; }
+        public string? Author { get; set; }
+    }
+}
